d1c0727c011b73f3a97e444601a699e0be6c89cf
Rodrigo Erthal <rrerthal@gmail.com>
2023-08-16 14:11:42 +0200

    update 16/08

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,23 @@
-
+class Game {
+    constructor(){
+       
+    }
+    start(){
+        this.attachEventListener();
+        
+    }
+    
+        attachEventListener(){
+            document.addEventListener("keydown", (event) => {
+                if (event.key === "ArrowLeft") {
+                    player.moveLeft();
+                } else if (event.key === "ArrowRight") {
+                    player.moveRight();
+                }
+            });
+        }
+    
+}
 
 
 
@@ -42,24 +61,19 @@ class Player {
 
 }
 
+
 const player = new Player();
 
 
 
-    document.addEventListener("keydown", (event) => {
-        if (event.key === "ArrowLeft") {
-            player.moveLeft();
-        } else if (event.key === "ArrowRight") {
-            player.moveRight();
-        }
-    });
+    
 
     class Bricks{
         constructor() {
-            this.width = 7
-            this.height = 2;
+            this.width = 5;
+            this.height = 1;
             this.positionX = 0;
-            this.positionY = -5;
+            this.positionY = 0;
             this.domElement = null;
             
     
@@ -68,16 +82,16 @@ const player = new Player();
         createDomElement() {
             
             this.domElement = document.createElement("div");
-            this.domElement.classList.add("brick");
+            
           
     
             // set id
-            this.domElement.id = "bricks";
+            this.domElement.className = "bricks";
             this.domElement.style.width = this.width + "vw";
             this.domElement.style.height = this.height + "vh";
             this.domElement.style.left = this.positionX + "vw";
             this.domElement.style.bottom = this.positionY + "vh";
-                 
+            
             //append to the dom
             const parentElm = document.getElementById("board");
             parentElm.appendChild(this.domElement);
@@ -89,4 +103,47 @@ const player = new Player();
     for (let i = 0; i < 32; i++) {
         const brick = new Bricks();
         }
+
+   class Ball{
+    constructor(){
+        this.width = 15;
+        this.height = 15;
+        this.positionX = (50 - (this.width / 2)) * (90 / 100);
+        this.positionY = 20;
+         
+
+
+        this.createDomElement();
+        this.moveDown();
+    }
+        createDomElement() {
+           
+            this.domElement = document.createElement("div");
+
+            this.domElement.id = "ball";
+            this.domElement.style.width = this.width + "px";
+            this.domElement.style.height = this.height + "px";
+            this.domElement.style.left = this.positionX + "vw";
+            this.domElement.style.bottom = this.positionY + "vh";
+
+            const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+     }
+
+     moveDown() {
+        setInterval(() => {
+            this.positionY++; // Mover a bola para baixo
+            this.domElement.style.bottom = this.positionY + "vh"; // Atualizar a posição na tela
+        }, 100); // Chame a função a cada 16ms para uma animação suave
+    }
+}
+
+
+
+
+
     
+ const ball = new Ball();
+
+ const game = new Game();
+ game.start();
